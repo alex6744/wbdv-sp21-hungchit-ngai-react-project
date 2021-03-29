dcea9240b9e97e721f7d96991491d9d757caca75
alex6744 <alexngai6744@gmail.com>
2021-03-29 15:19:31 -0700

    created TV service

---
+++ src/services/tv-service.js
@@ -0,0 +1,20 @@
+const MOVIE_URL="https://api.themoviedb.org/3/movie";
+const SEARCH_URL="https://api.themoviedb.org/3/search";
+const API_KEY="646b022f11ded757c770a0ee07929960";
+export const findMovieByTitle = (title) =>
+    fetch(`${SEARCH_URL}/movie?api_key=${API_KEY}&query=${title}`)
+        .then(response => response.json())
+
+export const findCreditById = (movieID) =>
+    fetch(`${MOVIE_URL}/${movieID}/credits?api_key=${API_KEY}`)
+        .then(response => response.json())
+
+export const findMovieById = (movieID) =>
+    fetch(`${MOVIE_URL}/${movieID}?api_key=${API_KEY}`)
+        .then(response => response.json())
+
+export default {
+    findMovieByTitle,
+    findMovieById,
+    findCreditById
+}
