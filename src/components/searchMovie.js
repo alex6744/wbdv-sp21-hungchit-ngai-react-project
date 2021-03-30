0a50ec0e7cd8079d557c049b55d69687892a1c38
alex6744 <alexngai6744@gmail.com>
2021-03-29 17:38:49 -0700

    rename

--- src/components/searchMovie.js
+++ src/components/searchMovie.js
@@ -2,46 +2,42 @@ import React,{useEffect,useState} from 'react'
 import {Link, useParams, useHistory} from "react-router-dom";
 import movieService from "../services/movie-service"
 
-const Search=()=>{
+const SearchMovie=()=>{
     const {title}=useParams()
     const [searchTitle,setSearchTitle]=useState("")
     const [results,setResults]=useState([])
     const history = useHistory()
     useEffect(()=>{
         setSearchTitle(title)
+
         if(title){
            movieService.findMovieByTitle(title)
                .then(results=>setResults(results.results))
         }
 
+
+
     },[title])
     return(
         <div>
-            <h1>Search</h1>
+            <h1>Search Movie</h1>
             <input onChange={event => {
                 setSearchTitle(event.target.value)
             }}
                     className="form-control"
                     value={searchTitle}/>
             <button onClick={()=> {
-                    history.push(`/search/${searchTitle}`)}}
+                    history.push(`/search/movie/${searchTitle}`)}}
                     className="btn btn-primary btn-block">
                 Search
             </button>
 
-            {/*{*/}
-            {/*    <button onClick={()=> {*/}
-            {/*        console.log(results)*/}
-            {/*        results.map(m=>console.log(m))}}*/}
-            {/*            className="btn btn-primary btn-block">*/}
-            {/*        Search*/}
-            {/*    </button>*/}
-            {/*}*/}
+
             <ul className="list-group">
                 {
                     results.map(movie=>
                         <li className="list-group-item" key={movie.id}>
-                            <Link to={`/details/${movie.id}`}>
+                            <Link to={`/details/movie/${movie.id}`}>
                                 {movie.original_title}
                             </Link>
                         </li>
@@ -49,7 +45,8 @@ const Search=()=>{
 
                 }
             </ul>
+
         </div>
     )
 }
-export default Search
+export default SearchMovie
