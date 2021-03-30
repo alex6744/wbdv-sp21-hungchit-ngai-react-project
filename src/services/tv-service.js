6c7b8cbab9794ffa6e0c82b6cfef37dfabc3a3d1
alex6744 <alexngai6744@gmail.com>
2021-03-29 17:39:28 -0700

    edit method

--- src/services/tv-service.js
+++ src/services/tv-service.js
@@ -1,20 +1,20 @@
-const MOVIE_URL="https://api.themoviedb.org/3/movie";
+const TV_URL="https://api.themoviedb.org/3/tv";
 const SEARCH_URL="https://api.themoviedb.org/3/search";
 const API_KEY="646b022f11ded757c770a0ee07929960";
-export const findMovieByTitle = (title) =>
-    fetch(`${SEARCH_URL}/movie?api_key=${API_KEY}&query=${title}`)
+export const findTvByTitle = (title) =>
+    fetch(`${SEARCH_URL}/tv?api_key=${API_KEY}&query=${title}`)
         .then(response => response.json())
 
-export const findCreditById = (movieID) =>
-    fetch(`${MOVIE_URL}/${movieID}/credits?api_key=${API_KEY}`)
+export const findCreditById = (TvID) =>
+    fetch(`${TV_URL}/${TvID}/credits?api_key=${API_KEY}`)
         .then(response => response.json())
 
-export const findMovieById = (movieID) =>
-    fetch(`${MOVIE_URL}/${movieID}?api_key=${API_KEY}`)
+export const findTvById = (TvID) =>
+    fetch(`${TV_URL}/${TvID}?api_key=${API_KEY}`)
         .then(response => response.json())
 
 export default {
-    findMovieByTitle,
-    findMovieById,
-    findCreditById
+    findCreditById,
+    findTvById,
+    findTvByTitle
 }
