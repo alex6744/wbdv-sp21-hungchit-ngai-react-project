46ae79599e8fe6b8ee5260ebd20dffd45b6ba394
alex6744 <alexngai6744@gmail.com>
2021-04-03 20:41:52 -0700

    created search

---
+++ src/components/search/search.js
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import {Link, Route, useHistory, useParams} from "react-router-dom";
+import SearchMovies from "./searchMovies";
+import SearchTV from "./searchTV";
+
+const Search=()=>{
+    const {title,type}=useParams()
+    const [searchTitle,setSearchTitle]=useState("")
+    const history = useHistory()
+    return(
+        <div>
+            <input  placeholder="search"
+                    className="form-control"
+                    onChange={(e)=>{
+                            setSearchTitle(e.target.value)
+                    }}/>
+            <button className="form-control"
+                    onClick={()=>{
+                        console.log(type)
+                        history.push(`/search/${type}/${searchTitle}`)
+                    }}>
+                Search
+            </button>
+            <br/>
+            <div className="row">
+                <div className="col-4">
+                    <ul className="list-group">
+                        <li className="list-group-item "  >
+                            <Link to={`/search/movie/${title}`} className="nav-link" >
+                                Movies
+                            </Link>
+
+                        </li>
+                        <li className="list-group-item ">
+                            <Link to={`/search/tv/${title}`} className="nav-link" >
+                                TV Shows
+                            </Link>
+                        </li>
+
+                    </ul>
+                </div>
+                <div className="col-8">
+                    <Route exact={true}
+                           path={["/search/movie","/search/movie/:title"]}>
+                        <SearchMovies/>
+                    </Route>
+                    <Route exact={true}
+                           path={["/search/tv","/search/tv/:title"]}>
+                        <SearchTV/>
+                    </Route>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+export default Search
