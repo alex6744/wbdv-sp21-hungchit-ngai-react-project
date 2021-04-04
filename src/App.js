11936972d9cf0bcc03441a6871541148fbb60e37
alex6744 <alexngai6744@gmail.com>
2021-04-03 20:39:36 -0700

    update link

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import {BrowserRouter,Route} from "react-router-dom";
 import Home from "./components/Home";
-import SearchMovie from "./components/searchMovie";
-import MovieDetails from "./components/movieDetails";
-import SearchTV from "./components/searchTV";
-import TvDetails from "./components/tvDetails";
 
+import MovieDetails from "./components/details/movieDetails";
+import SearchTV from "./components/search/searchTV";
+import TvDetails from "./components/details/tvDetails";
+import Main from "./components/manager"
+import Search from "./components/search/search";
+import Manager from "./components/manager";
 function App() {
   return (
     <div className="container-fluid">
       <BrowserRouter>
+        {/*<Route exact={true}*/}
+        {/*       path={["/"]}>*/}
+        {/*  <Home/>*/}
+        {/*</Route>*/}
+
+
         <Route exact={true}
-               path={["/"]}>
-          <Home/>
+               path={["/","/:layout","/:layout/:type","/:layout/:type/:title"]}>
+          <Manager/>
         </Route>
-        <Route exact={true}
-               path={["/search/movie","/search/movie/:title"]}>
-          <SearchMovie/>
-        </Route>
-        <Route exact={true}
-               path={["/search/tv","/search/tv/:title"]}>
-          <SearchTV/>
-        </Route>
-        <Route exact={true}
-               path={["/details/movie/:movieId"]}>
-          <MovieDetails/>
-        </Route>
-        <Route exact={true}
-               path={["/details/tv/:tvId"]}>
-          <TvDetails/>
-        </Route>
+
+
       </BrowserRouter>
     </div>
   );
