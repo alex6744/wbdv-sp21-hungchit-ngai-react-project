269634439da3b3ee860da814996df451cc083624
alex6744 <alexngai6744@gmail.com>
2021-04-12 22:46:50 -0700

    created register profile

---
+++ src/components/register/registerProfile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link,useParams} from "react-router-dom";
+const RegisterProfile=()=>{
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+export  default RegisterProfile
