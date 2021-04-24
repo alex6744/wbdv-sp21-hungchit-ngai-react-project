00cbf2e8970fdf5614da4f2f022860a6850ba699
alex6744 <alexngai6744@gmail.com>
2021-04-24 13:00:05 -0700

    add login detail

--- src/components/user/login.js
+++ src/components/user/login.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
+import LoginService, {userLogin} from "../../services/login-service"
+
+const Login=({loginUser,currentUser})=>{
+    const [user,setUser]=useState({username:"",password:""})
+    const [error,setError]=useState(false)
+    const [to,setTo]=useState("/login")
+    useEffect(()=>{
 
 
-const Login=()=>{
+
+    },[])
     return(
         <div className="container-fluid">
-            <h1>Login</h1>
+            <h1>Login   </h1>
+            <h4>{currentUser[0].error==="Unauthorized"&& "username or password is not correct , please try again"}</h4>
+
             <div className="mb-3 row">
                 <label htmlFor="username"
                        className="col-sm-2 col-form-label">
@@ -13,10 +23,13 @@ const Login=()=>{
                 </label>
                 <div className="col-sm-10">
                     <input type="text"
-
+                            value={user.username}
                            className="form-control"
                            id="username"
-                           placeholder="alice"/>
+                           placeholder="alice"
+                            onChange={(e)=>{
+                                setUser({...user,username:e.target.value})
+                            }}/>
                 </div>
             </div>
             <div className="mb-3 row">
@@ -26,17 +39,29 @@ const Login=()=>{
                 <div className="col-sm-10">
                     <input type="password"
                            className="form-control"
-                           id="inputPassword"/>
+                           id="inputPassword"
+                           value={user.password}
+                            onChange={(e)=>{
+                                setUser({...user,password:e.target.value})
+                            }}/>
                 </div>
             </div>
+            {JSON.stringify(user)}
+            {JSON.stringify(currentUser)}
             <div className="mb-3 row">
                 <label className="col-sm-2 col-form-label"></label>
                 <div className="col-sm-10">
-                    <Link to="/profile">
-                        <button className="btn btn-primary btn-block">
-                            Sign in
+                    <Link to={to}>
+                        <button className="btn btn-primary btn-block"
+                                onClick={()=>{
+                                    loginUser(user)
+                                }}>
+
+                                Sign in
+
                         </button>
                     </Link>
+
                     <div className="row">
                         <div className="col-6">
                             <a href="#">
@@ -51,6 +76,8 @@ const Login=()=>{
                     </div>
                 </div>
             </div>
+
+
         </div>
     )
 }
