f4d626596d755546b8a00d641f8fc48d650d9f0d
alex6744 <alexngai6744@gmail.com>
2021-04-26 19:16:53 -0700

    add links

--- src/components/manager.js
+++ src/components/manager.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link, Route,BrowserRouter} from "react-router-dom";
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
@@ -12,14 +12,31 @@ import Login from "./user/login";
 import Register from "./register/register";
 import Profile from "./user/profile";
 import LoginService, {updateUser} from "../services/login-service";
+import MovieNowPlaying from "./nowPlaying/moiveNowPlaying";
+import MoviePopular from "./popular/moviePopular";
+import TVPopular from "./popular/TVPopular";
+import TVNowPlaying from "./nowPlaying/TVNowPlaying";
+import ActorDetails from "./details/actorDetails";
 
-const Manager =({loginUser,currentUser,signout,updateUser})=>{
+const Manager =({history})=>{
+
+    useEffect(()=>{
+
+    },[])
 
         return(
             <div>
+                <button onClick={()=>{
+                    LoginService.currentUser().then(user=>console.log(user))
+                }}>sds</button>
+                <button onClick={()=>{
+                    LoginService.profile().then(u=>console.log(u))
+                }}>sds</button>
                 {JSON.stringify("sds")}
-                {JSON.stringify(currentUser)}
 
+                {localStorage.getItem("username")}
+                {localStorage.getItem("token")}
+                {localStorage.getItem("roles")}
                 <nav className="navbar navbar-expand-lg navbar-light bg-light container-fluid navbar-style">
                     <div className="container-fluid">
 
@@ -32,8 +49,17 @@ const Manager =({loginUser,currentUser,signout,updateUser})=>{
                         <div className="col-1 ">
                             <DropdownButton variant="white" id="dropdown-basic-button" title="Movies">
 
-                                <Dropdown.Item href="#/action-2">Now playing</Dropdown.Item>
-                                <Dropdown.Item href="#/action-3">popular</Dropdown.Item>
+                                <Dropdown.Item >
+                                    <Link to={"/nowplaying/movie"}>
+                                        Now playing
+                                    </Link>
+                                </Dropdown.Item>
+                                <Dropdown.Item >
+                                    <Link to={"/popular/movie"}>
+                                        Popular
+                                    </Link>
+
+                                </Dropdown.Item>
                                 <Dropdown.Divider />
                                 <Dropdown.Item >
                                     <Link to="/search/movie">
@@ -46,9 +72,22 @@ const Manager =({loginUser,currentUser,signout,updateUser})=>{
                         </div>
                         <div className="col-1 ">
                             <DropdownButton variant="white" id="dropdown-basic-button" title="TV shows">
-                                <Dropdown.Item href="#/action-1">Now playing</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">popular</Dropdown.Item>
-                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                <Dropdown.Item >
+                                    <Link to={"/nowplaying/tv"}>
+                                        Now playing
+                                    </Link>
+                                </Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">
+                                    <Link to={"/popular/tv"}>
+                                        Popular
+                                    </Link>
+                                </Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item >
+                                    <Link to="/search/tv">
+                                        Search TV shows
+                                    </Link>
+                                </Dropdown.Item>
                             </DropdownButton>
                         </div>
                         <div className="col-1 ">
@@ -66,16 +105,16 @@ const Manager =({loginUser,currentUser,signout,updateUser})=>{
                         <div className="col-1 ">
 
                             {
-                                !currentUser[0].id&&
+                                !localStorage.getItem("username")&&
                                 <Link to={"/login"}>
                                 login
                                 </Link>
                             }
                             {
-                                currentUser[0].id&&
+                                localStorage.getItem("username")&&
                                 <Link to={"/profile"}>
-                                    {/*{currentUser[0].username}*/}
-                                    sadsds
+
+                                    {localStorage.getItem("username")}
                                 </Link>
                             }
 
@@ -83,17 +122,17 @@ const Manager =({loginUser,currentUser,signout,updateUser})=>{
                         </div>
                         <div className="col-1 ">
                             {
-                                !currentUser[0].id&&
+                                !localStorage.getItem("username")&&
                                 <Link to={"/register"}>
                                 sign up
                                 </Link>
                             }
                             {
-                               currentUser[0].id&&
-                              <div onClick={()=> signout()}>
-                                <Link to={"/main"}>
+                                localStorage.getItem("username")&&
+                              <div onClick={()=> localStorage.clear()}>
+                                <a href={"/home"}>
                                    sign out
-                                </Link>
+                                </a>
                               </div>
                             }
                         </div>
@@ -110,34 +149,49 @@ const Manager =({loginUser,currentUser,signout,updateUser})=>{
                        exact={true}>
                     <Main/>
                 </Route>
-
+                <Route path={["/popular/movie"]}
+                       exact={true}>
+                    <MoviePopular/>
+                </Route>
+                <Route path={["/popular/tv"]}
+                       exact={true}>
+                    <TVPopular/>
+                </Route>
+                <Route path={["/nowplaying/tv"]}
+                       exact={true}>
+                    <TVNowPlaying/>
+                </Route>
+                <Route path={["/nowplaying/movie"]}
+                       exact={true}>
+                    <MovieNowPlaying/>
+                </Route>
                 <Route path={["/search","/search/:type","/search/:type/:title"]}
                        exact={true}>
                     <Search/>
                 </Route>
                 <Route path={["/details/movie/:title"]}
                        exact={true}>
-                    <MovieDetails currentUser={currentUser}/>
+                    <MovieDetails />
                 </Route>
                 <Route path={["/details/tv/:title"]}
                        exact={true}>
                     <TvDetails/>
                 </Route>
+                <Route path={["/details/actor/:title"]}
+                       exact={true}>
+                    <ActorDetails/>
+                </Route>
                 <Route path={["/login"]}
                        exact={true}>
-                    <Login loginUser={loginUser}
-                           currentUser={currentUser}/>
+                    <Login  history={history}/>
                 </Route>
                 <Route path={["/register"]}
                        exact={true}>
-                    <Register/>
+                    <Register history={history}/>
                 </Route>
                 <Route path={["/profile"]}
                        exact={true}>
-                    <Profile updateUser={updateUser}
-                             currentUser={currentUser}
-
-                    />
+                    <Profile/>
                 </Route>
 
 
@@ -148,19 +202,5 @@ const Manager =({loginUser,currentUser,signout,updateUser})=>{
 
 
 }
-const stpm = (state) => ({
-    currentUser: state.userReducer.currentUser
-})
-const dtpm=(dispatch)=>({
-    loginUser:(user)=>{
-        LoginService.userLogin(user).then(feedback=>dispatch({type:"LOGIN",user:feedback}))
-    },
-    signout:()=>{
-       dispatch({type:"SIGNOUT"})
-    },
-    updateUser:(token,user)=>{
-        LoginService.updateUser(token,user).then(feedback=>dispatch({type:"UPDATE",user:user}))
-    }
 
-})
-export default connect(stpm,dtpm)(Manager)
+export default Manager
