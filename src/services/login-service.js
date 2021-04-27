31284330cfd593a3757fdcad2ffbea0f3c8dd1f6
alex6744 <alexngai6744@gmail.com>
2021-04-26 19:16:38 -0700

    add login detail

--- src/services/login-service.js
+++ src/services/login-service.js
@@ -1,42 +1,82 @@
 const LOGIN_URL="http://localhost:8080";
 //const LOGIN_URL="https://hungchit-ngai-java-server-01.herokuapp.com";
 
-export const userRegister=(user)=>
-    fetch(`${LOGIN_URL}/api/auth/signup`, {
+export const userRegister=(user)=> {
+
+   return  fetch(`${LOGIN_URL}/api/auth/signup`, {
         method: "POST",
+
         body: JSON.stringify(user),
         headers: {
             'content-type': 'application/json'
         }
     })
         .then(response => response.json())
-
-export const userLogin=(user)=>
-    fetch(`${LOGIN_URL}/api/auth/signin`, {
+}
+export const userLogin=(user)=> {
+    return fetch(`${LOGIN_URL}/api/auth/signin`, {
         method: "POST",
+
         body: JSON.stringify(user),
         headers: {
             'content-type': 'application/json'
         }
     })
         .then(response => response.json())
+}
 
-export const updateUser=(user)=>
+export const updateUser=(user,token)=> {
 
-    fetch(`${LOGIN_URL}/api/controller/${user.id}`, {
-        credentials: "include",
+    return fetch(`${LOGIN_URL}/api/controller/profile/${user.id}`, {
+
         method: "PUT",
+
         body: JSON.stringify(user),
         headers: {
 
-            'content-type': 'application/json'
+            'content-type': 'application/json',
+            'Authorization':'Bearer '+token
         },
 
 
     })
         .then(response => response.json())
+}
 
+export const signout=()=>
+    fetch(`${LOGIN_URL}/api/controller/signout`,{
+        method:"GET",
+
+        headers: {
+
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+export const profile=(id,token)=>
+    fetch(`${LOGIN_URL}/api/controller/profile/${id}`,{
+        method:"GET",
+
+        headers: {
+
+            'content-type': 'application/json',
+            'Authorization':'Bearer '+token
+        }
+    }).then(response => response.json())
+
+export const currentUser=()=>{
+    return  fetch(`${LOGIN_URL}/api/controller/currentUser`,{
+        method:"GET",
+        headers: {
+
+            'content-type': 'application/json'
+
+
+        }
+    })
+        .then(response=>response.json())
+}
 export default {
+    signout,profile,currentUser,
     userLogin,
     userRegister,updateUser
 }
